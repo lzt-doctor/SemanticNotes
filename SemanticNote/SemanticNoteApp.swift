614917b07c11691948c1877258bedcef7fe23956
嘟嘟 <356464491@qq.com>
2026-07-07 20:08:05 +0800

    Initial Commit

---
+++ SemanticNote/SemanticNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SemanticNoteApp.swift
+//  SemanticNote
+//
+//  Created by 脑洞大开 on 2026/7/7.
+//
+
+import SwiftUI
+
+@main
+struct SemanticNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
